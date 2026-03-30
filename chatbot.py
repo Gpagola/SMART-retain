89f2dba969acb922066b97081576fe16bc81bb36
Gabriel Pagola <gpagola@gmail.com>
2026-03-29 23:13:16 -0300

    1.3.1 nuevo repo

--- chatbot.py
+++ chatbot.py
@@ -28,7 +28,7 @@ from langgraph.prebuilt import ToolNode
 from langgraph.checkpoint.base import BaseCheckpointSaver, CheckpointTuple
 
 
-# ── Carga de variables de entorno ────────────────────────────────────────────
+# ── Carga de variab les de entorno ────────────────────────────────────────────
 load_dotenv()
 
 DB_CONFIG = {
